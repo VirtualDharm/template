9baa4d7e972c273505072affb00f99c690331979
VirtualDharm <mdharm4air.fm@gmail.com>
2024-07-26 14:36:58 +0530

    adding pd

--- backend/djangobackend/myapp/views.py
+++ backend/djangobackend/myapp/views.py
@@ -2,15 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 # myapp/views.py
-
+import pandas as pd
 from django.http import JsonResponse
 from django.db import connection
 
 def get_data(request):
     with connection.cursor() as cursor:
-        query = "SELECT * FROM mytable limit 5"
-        cursor.execute(query)
-        columns = [col[0] for col in cursor.description]
-        results = [dict(zip(columns, row)) for row in cursor.fetchall()]
-
-    return JsonResponse(results, safe=False)
+        query = "SELECT * FROM mytable LIMIT 5"
+        df = pd.read_sql_query(query, connection)
+        results = df.to_dict(orient='records')
+    return JsonResponse(results, safe=False, json_dumps_params={'indent': 2})
